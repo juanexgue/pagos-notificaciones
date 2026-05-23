280a961f9e73c57005e5e245a9fea4fb740934e2
Juan <juanexgue@gmail.com>
2026-05-23 13:54:56 -0500

    feat: add entity Customer

---
+++ src/main/java/com/juanex/customer_service/entity/Customer.java
@@ -0,0 +1,23 @@
+package com.juanex.customer_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "customers")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+}
